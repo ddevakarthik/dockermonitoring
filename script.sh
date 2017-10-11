5a7600e14c4105851301cc4447b49e0c10cd70cb
devakarthik <devakarthik@gmail.com>
2017-10-11 14:08:09 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Install Docker
+apt-get update
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository "deb https://apt.dockerproject.org/repo ubuntu-xenial main"
+apt-get update
+apt-get -y install docker-engine
+usermod -aG docker "$USER"
+systemctl enable docker
+
+#Install Python Installer and Docker-Compose
+apt-get -y install python-pip
+pip install docker-compose
+
+#install git
+apt-get install git
+
+#Clone the repository
+git clone https://github.com/ddevakarthik/continuousmonitoring.git
+
+#Create prometheus configuration
+cd continuousmonitoring
+mkdir -p /var/prometheus
+mv prometheus.yml /var/prometheus/
+
+#Docker Compose run
+docker-compose up -d
